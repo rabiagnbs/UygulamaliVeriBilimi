18fa66014225545140ee3d8491e4e6b64035d42b
rabiagnbs <rabiagnbs@gmail.com>
2024-12-06 15:24:17 +0300

    CART(Karar Ağaçları)

--- diabets/Cart(KararAgaclari).py
+++ diabets/Cart(KararAgaclari).py
@@ -15,6 +15,11 @@
 # 11. Prediction using Python Codes
 # 12. Saving and Loading Model
 
+# pip install pydotplus
+# pip install skompiler
+# pip install astor
+# pip install joblib
+
 import warnings
 import joblib
 import pydotplus
@@ -27,6 +32,7 @@ from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate, validation_curve
 from skompiler import skompile
 import graphviz
+
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
@@ -44,4 +50,250 @@ warnings.simplefilter(action='ignore', category=Warning)
 # 3. Modeling using CART
 ################################################
 
-df = pd.read_csv("datasets/diabetes.csv")
+df = pd.read_csv("/Users/rabiagnbs/Desktop/Code/VeriBilimi/PycharmProjects/pythonProject/diabets/diabetes.csv")
+
+y=df["Outcome"]
+X= df.drop(["Outcome"], axis=1)
+
+cart_model = DecisionTreeClassifier(random_state=1).fit(X, y)
+
+y_pred = cart_model.predict(X)
+y_prob =cart_model.predict_proba(X)[:, 1]
+
+print(classification_report(y, y_pred))
+roc_auc_score(y, y_prob)
+#Sorumuz şu başarımı nasıl daha doğru değerlendirebilirim?
+
+#####################
+# Holdout Yöntemi ile Başarı Değerlendirme
+#####################
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+cart_model=DecisionTreeClassifier(random_state=17).fit(X_train, y_train)
+
+y_pred = cart_model.predict(X_train)
+y_prob =cart_model.predict_proba(X_train)[:, 1]
+
+print(classification_report(y_train, y_pred))
+roc_auc_score(y_train, y_prob)
+
+y_pred = cart_model.predict(X_test)
+y_prob =cart_model.predict_proba(X_test)[:, 1]
+
+print(classification_report(y_test, y_pred))
+roc_auc_score(y_test, y_prob)
+
+#Train 1 çıkmasına rağmen test doğruluğu düşük çıktığı için overfit olduğunu anlıyoruz.Bu nedenle çapraz doğrulama yapmamaız gerekiyor.
+
+#####################
+# CV ile Başarı Değerlendirme
+#####################
+
+cart_model=DecisionTreeClassifier(random_state=17).fit(X_train, y_train)
+cv_results=cross_validate(cart_model, X, y, cv=10, scoring=["accuracy", "f1" ,"roc_auc"])
+cv_results["test_accuracy"].mean()
+cv_results["test_f1"].mean()
+cv_results["test_roc_auc"].mean()
+
+#dengesiz veri yaklaşımları başarımızı arttırabilir.
+
+################################################
+# 4. Hyperparameter Optimization with GridSearchCV
+################################################
+
+cart_model.get_params()
+
+cart_params = {'max_depth': range(1, 11),
+               "min_samples_split": range(2, 20)}
+
+cart_best_grid = GridSearchCV(cart_model,
+                              cart_params,
+                              scoring="roc_auc",
+                              cv=5,
+                              n_jobs=-1,
+                              verbose=1).fit(X, y)
+cart_best_grid.best_params_
+
+cart_best_grid.best_score_
+
+random= X.sample(1, random_state=45)
+cart_best_grid.predict(random)
+
+################################################
+# 5. Final Model
+################################################
+
+cart_final =DecisionTreeClassifier(**cart_best_grid.best_params_, random_state=17).fit(X,y)
+cart_final = cart_model.set_params(**cart_best_grid.best_params_).fit(X,y)
+cart_final.get_params()
+
+cv_results=cross_validate(cart_final, X, y, cv=10, scoring=["accuracy", "f1" ,"roc_auc"])
+cv_results["test_accuracy"].mean()
+cv_results["test_f1"].mean()
+cv_results["test_roc_auc"].mean()
+
+################################################
+# 6. Feature Importance
+################################################
+#Elimizdeki değişkenlerin sağladığı katkılara göre önemlerini sıralayacağız.
+
+cart_final.feature_importances_ #değişkenlerin önem düzeylerini belirtir.
+
+#Elimizdeki veri setindeki değişkenlerinin önemini görselleştiren fonksiyon oluşturuyoruz.
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('importances.png')
+
+
+plot_importance(cart_final, X)
+
+################################################
+# 7. Analyzing Model Complexity with Learning Curves (BONUS)
+################################################
+#model başarısını değerlendirmek için train ve test ayrımına belli bir parametre için ve belli bir ölçüte göre(scoring) validation_curve yöntemi ile yaparız.
+
+train_score, test_score= validation_curve(cart_final, X, y,
+                                          param_name='max_depth',
+                                          param_range=range(1,11),
+                                          scoring="roc_auc",
+                                          cv=10)
+
+#Validasyona bazen test bazen validasyon olarak adlandırılır. Train her zaman train'dir.
+
+mean_train_score = np.mean(train_score, axis=1)
+mean_test_score = np.mean(test_score, axis=1)
+
+
+plt.plot(range(1, 11), mean_train_score,
+         label="Training Score", color='b')
+
+plt.plot(range(1, 11), mean_test_score,
+         label="Validation Score", color='g')
+
+plt.title("Validation Curve for CART")
+plt.xlabel("Number of max_depth")
+plt.ylabel("AUC")
+plt.tight_layout()
+plt.legend(loc='best')
+plt.show()
+
+#eğer tek başına max_depth özelliğine bakarak en doğru paramtere değerlerine göre model eğitseydik
+#buradaki sonuca göre yapardık ama biz GridSearch ile birden bazla parametre birlikte bulunduğundaki en iyi sonuçları aldığımız için burada sadece kontrol etmiş oluyoruz.
+
+def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
+    train_score, test_score = validation_curve(
+        model, X=X, y=y, param_name=param_name, param_range=param_range, scoring=scoring, cv=cv)
+
+    mean_train_score = np.mean(train_score, axis=1)
+    mean_test_score = np.mean(test_score, axis=1)
+
+    plt.plot(param_range, mean_train_score,
+             label="Training Score", color='b')
+
+    plt.plot(param_range, mean_test_score,
+             label="Validation Score", color='g')
+
+    plt.title(f"Validation Curve for {type(model).__name__}")
+    plt.xlabel(f"Number of {param_name}")
+    plt.ylabel(f"{scoring}")
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.show(block=True) #block true ile değişkenleri farklı grafiklerde görselleştiririz.
+
+
+val_curve_params(cart_final, X, y, "max_depth", range(1, 11), scoring="f1")
+
+cart_val_params = [["max_depth", range(1, 11)], ["min_samples_split", range(2, 20)]]
+
+for i in range(len(cart_val_params)):
+    val_curve_params(cart_model, X, y, cart_val_params[i][0], cart_val_params[i][1])
+
+#bütün parametreler için yapmak için yukarıdaki gibi bir fonksiyon belirlendi.
+
+################################################
+# 8. Visualizing the Decision Tree
+################################################
+
+#Karar ağacını görselleştirmek istersek bunları yaparız:
+import graphviz
+
+def tree_graph(model, col_names, file_name):
+    tree_str = export_graphviz(model, feature_names=col_names, filled=True, out_file=None)
+    graph = pydotplus.graph_from_dot_data(tree_str)
+    graph.write_png(file_name)
+
+
+tree_graph(model=cart_final, col_names=X.columns, file_name="cart_final.png")
+
+cart_final.get_params()
+
+################################################
+# 9. Extracting Decision Rules
+################################################
+#Karar kurallarını konsolda bu şekilde görüntüleriz.
+tree_rules = export_text(cart_final, feature_names=list(X.columns))
+print(tree_rules)
+
+################################################
+# 10. Extracting Python Codes of Decision Rules
+################################################
+
+#Karar kurallarının python, sql ve excel kodlarını çıkarma işlemini uygulayacağız.
+# sklearn '0.23.1' versiyonu ile yapılabilir.
+# pip install scikit-learn==0.23.1
+#modeli pythonda bu kodları kullanarak çalıştırabileceğiz.
+print(skompile(cart_final.predict).to('python/code'))
+
+print(skompile(cart_final.predict).to('sqlalchemy/sqlite'))
+
+print(skompile(cart_final.predict).to('excel'))
+
+################################################
+# 11. Prediction using Python Codes
+################################################
+
+#Python kodlarını kullanarak tahmin işlemi yapacağız.
+
+def predict_with_rules(x):
+    return (((((0 if x[0] <= 7.5 else 1) if x[5] <= 30.949999809265137 else 0 if x[6] <=
+    0.5005000084638596 else 0) if x[5] <= 45.39999961853027 else 1) if x[7] <=
+    28.5 else (1 if x[5] <= 9.649999618530273 else 0) if x[5] <=
+    26.350000381469727 else (1 if x[1] <= 28.5 else 0) if x[1] <= 99.5 else
+    0 if x[6] <= 0.5609999895095825 else 1) if x[1] <= 127.5 else (((0 if x
+    [5] <= 28.149999618530273 else 1) if x[4] <= 132.5 else 0) if x[1] <=
+    145.5 else 0 if x[7] <= 25.5 else 1 if x[7] <= 61.0 else 0) if x[5] <=
+    29.949999809265137 else ((1 if x[2] <= 61.0 else 0) if x[7] <= 30.5 else
+    1 if x[6] <= 0.4294999986886978 else 1) if x[1] <= 157.5 else (1 if x[6
+    ] <= 0.3004999905824661 else 1) if x[4] <= 629.5 else 0)
+
+X.columns
+
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+
+predict_with_rules(x)
+
+x = [6, 148, 70, 35, 0, 30, 0.62, 50]
+
+predict_with_rules(x)
+
+################################################
+# 12. Saving and Loading Model
+################################################
+
+#Model kaydetme ve gerektiğinde bu kodları çağırma işlemi yapacağız.
+joblib.dump(cart_final, "cart_final.pkl")
+
+cart_model_from_disc = joblib.load("/Users/rabiagnbs/Desktop/Code/VeriBilimi/PycharmProjects/pythonProject/diabets/cart_final.pkl")
+
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+
+cart_model_from_disc.predict(pd.DataFrame(x).T)
